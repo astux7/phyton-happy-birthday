69f450fd81a6d4431704ace874b1d3cdedebb1e4
astux7 <Asta.bevainyte@gmail.com>
2015-07-26 19:13:42 +0100

    OOP happy birthaday message

--- test.py
+++ test.py
@@ -2,43 +2,74 @@
 import time
 import os
 
-for num in range(0,23):
-  os.system('clear')
-  print "   Happy Birthday!"
-  print "                     "
-  if num > 10:
-    print "   Make a Wish..."
-  else:
-    print ""
-  if num > 13:
-    print "   Start counting ... "
-  else:
-    print ""
-  if num > 15:
-    print "        3"
-  else:
-    print ""
-  if num > 17:
-    print "        2 "
-  else:
-    print ""
-  if num > 19:
-    print "        1 "
-  else:
-    print ""
-  if num < 21:
-    print ""
-    if num%2 == 0: 
-      print "    *    *     *  "
+class Cake:
+  'Class of the cake'
+   
+  def displayCake(self):
+    print "   ||   ||    ||   "
+    print "(=================)"
+    print "( *************** )"
+    print "(_________________)"
+
+class Candles:
+  'Class of the candles'
+   
+  def flame(self, num):
+    if num < 21:
+      if num%2 == 0: 
+        print "  *    *     *  "
+      else:
+        print "   *    *     *  "
     else:
-      print "     *    *     *  "
-  else:
+      print ""
+
+class BirthdayText:
+  "Birthday text"
+
+  def birthdayMessage(self):
+    print "   Happy Birthday!"
     print ""
-    print ""
-  print "     ||   ||    ||  "
-  print "  (===============)"
-  print "  ( ************* )"
-  print "  (_______________)"
+
+  def wishMessage(self, num):
+    if num > 10:
+      print "   Make a Wish..."
+    else:
+      print ""
+
+  def countingMessage(self, num):
+    if num > 13:
+      print "   Start counting ... "
+    else:
+      print ""
+    if num > 15:
+      print "        3"
+    else:
+      print ""
+    if num > 17:
+      print "        2 "
+    else:
+      print ""
+    if num > 19:
+      print "        1 "
+    else:
+      print ""
+
+text = BirthdayText()
+
+for num in range(0,23):
+
+  os.system('clear')
+
+  text.birthdayMessage()
+
+  text.wishMessage(num)
+  
+  text.countingMessage(num)
+
+  Candles().flame(num)
+
+  Cake().displayCake()
+
   print ""
- 
+
   time.sleep(1)
